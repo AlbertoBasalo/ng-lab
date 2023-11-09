8acde3bedbe401057fbd1ee2a0882c8838320ea7
Alberto Basalo <albertobasalo@hotmail.com>
2023-11-09 14:09:29 +0100

    feat: register form

---
+++ src/app/routes/auth/register.form.ts
@@ -0,0 +1,76 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+
+@Component({
+  selector: 'lab-register',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  template: `
+    <form [formGroup]="form">
+      <fieldset>
+        <label for="username">
+          <span>User name</span>
+          <small id="username-error"> We need your name</small>
+          <input
+            type="text"
+            id="username"
+            formControlName="username"
+            autocomplete="cc-name"
+            [attr.aria-invalid]="form.controls['username'].invalid"
+          />
+        </label>
+        <label for="email">
+          <span>Email</span>
+          <small id="email-error"> We need your email</small>
+          <input
+            type="email"
+            id="email"
+            formControlName="email"
+            autocomplete="off"
+            [attr.aria-invalid]="form.controls['email'].invalid"
+          />
+        </label>
+        <label for="password">
+          <span>Password</span>
+          <small id="password-error"> At least 4 chars long</small>
+          <input
+            type="password"
+            id="password"
+            formControlName="password"
+            autocomplete="new-password"
+            [attr.aria-invalid]="form.controls['password'].invalid"
+          />
+        </label>
+        <label for="repeatPassword">
+          <span>Repeat your Password</span>
+          <small id="repeatPassword-error"> Must match</small>
+          <input
+            type="password"
+            id="repeatPassword"
+            formControlName="repeatPassword"
+            [attr.aria-invalid]="form.controls['repeatPassword'].invalid"
+          />
+        </label>
+      </fieldset>
+      <button type="submit" [disabled]="form.invalid">Register</button>
+      <input type="reset" value="Reset form" />
+    </form>
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class RegisterForm {
+  readonly #passwordValidators = [Validators.required, Validators.minLength(4)];
+
+  form = new FormGroup({
+    username: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', this.#passwordValidators),
+    repeatPassword: new FormControl('', this.#passwordValidators),
+  });
+}
--- src/app/routes/auth/register.page.ts
+++ src/app/routes/auth/register.page.ts
@@ -1,11 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RegisterForm } from './register.form';
 
 @Component({
-  selector: 'lab-register',
   standalone: true,
-  imports: [CommonModule],
-  template: ` <p>register works!</p> `,
+  imports: [CommonModule, RegisterForm],
+  template: `
+    <article>
+      <header>
+        <h2>Register</h2>
+      </header>
+      <main>
+        <lab-register />
+      </main>
+    </article>
+  `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export default class RegisterPage {}
