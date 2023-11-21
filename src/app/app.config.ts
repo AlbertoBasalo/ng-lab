8a7656c516f0e548b789ae22e038576be247cb31
Alberto Basalo <albertobasalo@hotmail.com>
2023-11-21 13:38:47 +0100

    feat: slug page

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,13 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideClientHydration } from '@angular/platform-browser';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideHttpClient(),
+    provideHttpClient(withFetch()),
     provideRouter(routes, withComponentInputBinding()),
     provideClientHydration(),
   ],
--- src/app/routes/activities/slug/activity-slug.component.ts
+++ src/app/routes/activities/slug/activity-slug.component.ts
@@ -1,23 +1,38 @@
-import { CurrencyPipe, DatePipe } from '@angular/common';
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
-import { Activity } from '../../../shared/activity.type';
+import { CurrencyPipe, DatePipe, TitleCasePipe } from '@angular/common';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+  computed,
+  signal,
+} from '@angular/core';
+import { Activity, NULL_ACTIVITY } from '../../../shared/activity.type';
 
 @Component({
   selector: 'lab-activity-slug',
   standalone: true,
-  imports: [CurrencyPipe, DatePipe],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [CurrencyPipe, DatePipe, TitleCasePipe],
   template: `
     <article name="Activity details">
       <header>
-        <h2>{{ activity.name }}</h2>
-        <p>Book your activity now!</p>
+        <h2>{{ title() | titlecase }}</h2>
+        <p>{{ subtitle }}</p>
       </header>
       <section>
-        <p>Price: {{ activity.price | currency: 'EUR' }}</p>
-        <p>Date: {{ activity.date | date: 'fullDate' }}</p>
         <p>
-          Participants: min: {{ activity.minParticipants }} max:
-          {{ activity.maxParticipants }}
+          Price: <span class="data">{{ state().price | currency: 'EUR' }}</span>
+        </p>
+        <p>
+          Date: <span class="data">{{ state().date | date: 'fullDate' }}</span>
+        </p>
+        <p>
+          Minimum Participants:
+          <span class="data">{{ state().minParticipants }}</span>
+        </p>
+        <p>
+          Maximum Capacity:
+          <span class="data">{{ state().maxParticipants }}</span>
         </p>
       </section>
       <footer>
@@ -25,8 +40,18 @@ import { Activity } from '../../../shared/activity.type';
       </footer>
     </article>
   `,
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  styles: `
+    .data {
+      font-weight: bold;
+    }
+  `,
 })
 export class ActivitySlugComponent {
-  @Input({ required: true }) activity!: Activity;
+  @Input({ required: true })
+  set activity(activity: Activity) {
+    this.state.set(activity);
+  }
+  state = signal<Activity>(NULL_ACTIVITY);
+  title = computed(() => this.state().name);
+  subtitle = 'Book your activity now!';
 }
