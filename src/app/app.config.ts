7e4e0a5fc67fb4afb63626eaad7ffb4e4dc9aa5e
Alberto Basalo <albertobasalo@hotmail.com>
2023-11-27 09:59:26 +0100

    clea: error service

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, ErrorHandler } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import {
@@ -8,11 +8,12 @@ import {
 } from '@angular/common/http';
 import { provideClientHydration } from '@angular/platform-browser';
 import { AuthInterceptor } from '@core/auth/auth.interceptor';
+import { ErrorService } from '@core/error.service';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    //  { provide: ErrorHandler, useClass: ErrorService },
+    { provide: ErrorHandler, useClass: ErrorService },
     provideHttpClient(withFetch(), withInterceptors([AuthInterceptor])),
 
     provideRouter(routes, withComponentInputBinding()),
