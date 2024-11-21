c3fce0fd0a10f4d83c6bf6b750cdea3e0c3c30b8
Alberto Basalo <albertobasalo@hotmail.com>
2024-11-21 09:33:23 +0100

    feat!: ng update 19

--- AstroBookings/src/app/app.component.ts
+++ AstroBookings/src/app/app.component.ts
@@ -2,15 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'lab-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  template: `
+    selector: 'lab-root',
+    imports: [RouterOutlet],
+    template: `
     <h1>Welcome to {{title}}!</h1>
 
     <router-outlet />
   `,
-  styles: [],
+    styles: []
 })
 export class AppComponent {
   title = 'AstroBookings';
